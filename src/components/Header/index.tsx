24a40db783045fd459661d123ebd71dab21931a6
WalleksMR <walleksmiranda@gmail.com>
2021-04-23 14:31:56 -0300

    Pages constructed

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM',{
+    locale: ptBR,
+  })
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a><img src="/logo.svg" alt="podcastr"/></a>
+      </Link>
+      
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
